b981a1f90d180f35847a673b2415108d889970dd
Anshit Chauhan <chauhananshit48@gmail.com>
2026-03-05 11:25:14 +0530

    feat: add next link route to navigate to test page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Dashboard from "@/components/Dashboard/Dashboard";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-gray-900">
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
       <Dashboard />
     </div>
   );
--- components/TestConfig/StartButton.tsx
+++ components/TestConfig/StartButton.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link"
+
 const StartButton = () => {
   return (
     <div>
-        <button className="px-4 py-2 m-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">Start Practice</button>
+        <Link href="/practice"> 
+          <button className="px-4 py-2 m-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">Start Practice</button>
+        </Link>
     </div>
   )
 }
