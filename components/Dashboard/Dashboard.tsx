9ad003f9c759478d2abe42f3efcdefb4bf99e346
Anshit Chauhan <chauhananshit48@gmail.com>
2026-03-05 11:07:14 +0530

    refac: modified the styling in dashboard

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -7,7 +7,7 @@ const Dashboard = () => {
     <div>
         <h3>Typing Master</h3>
         <p>Master your keyboard. Test and improve your typing speed.</p>
-        <div>
+        <div className="border  p-4 m-4">
             Configure your test
             <TimeSelector />
             <ModeSelector />
--- components/TestConfig/StartButton.tsx
+++ components/TestConfig/StartButton.tsx
@@ -1,7 +1,7 @@
 const StartButton = () => {
   return (
     <div>
-        <button className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">Start Practice</button>
+        <button className="px-4 py-2 m-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">Start Practice</button>
     </div>
   )
 }
