6657bee974c83968260e627b4aa152c7af958857
Anshit Chauhan <chauhananshit48@gmail.com>
2026-03-05 16:24:53 +0530

    feat: add svg to navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,3 +1,6 @@
+import { FaKeyboard } from "react-icons/fa";
+import { IoStatsChartOutline } from "react-icons/io5";
+import { GiPlagueDoctorProfile } from "react-icons/gi";
 
 const Navbar = () => {
   return (
@@ -5,17 +8,17 @@ const Navbar = () => {
       <div className="flex items-center justify-between max-w-7xl mx-auto">
         {/* Left: Logo + Webpage Name */}
         <div className="flex items-center space-x-3">
-          o
-          <span className="text-2xl font-bold text-gray-900">MyWebApp</span>
+          <FaKeyboard />
+          <span className="text-2xl font-bold text-gray-900">KeyVelocity</span>
         </div>
 
         {/* Right: Icons */}
         <div className="flex items-center space-x-4">
           <button className="p-2 text-gray-500 hover:text-gray-900 hover:bg-gray-100 rounded-lg transition-all duration-200">
-            o
+            <IoStatsChartOutline />
           </button>
           <button className="p-2 text-gray-500 hover:text-gray-900 hover:bg-gray-100 rounded-lg transition-all duration-200">
-            o
+            <GiPlagueDoctorProfile />
           </button>
         </div>
       </div>
