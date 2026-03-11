8bb4864fdb3faa969518bbc5efd57f9583027786
Anshit Chauhan <chauhananshit48@gmail.com>
2026-03-11 15:02:20 +0530

    fix: load Google AdSense script before interactive for site verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7496281420162166"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
         />
       </head>
       <body
